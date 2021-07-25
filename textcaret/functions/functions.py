fbc1c1b9d071f2d1ded901ead40572f39b51b76c
JCharis <jcharistech@gmail.com>
2021-07-25 14:28:37 +0300

    Added TextVisualization

--- textcaret/functions/functions.py
+++ textcaret/functions/functions.py
@@ -1,73 +1,150 @@
-from collections import Counter,defaultdict
-import matplotlib.pyplot as plt 
+# -*- coding: utf-8 -*-
+# This file is part of the NEAT Project suite of libraries
+# Please see the LICENSE file that should have been included as part of this
+# package.
+
+
+from collections import Counter, defaultdict
+import matplotlib.pyplot as plt
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
 from sumy.summarizers.lsa import LsaSummarizer
 from sumy.summarizers.luhn import LuhnSummarizer
 import random
-
-
-def get_most_common_tokens(docx,num=10):
-	word_freq = Counter(docx.split())
-	most_common_tokens = word_freq.most_common(num)
-	return dict(most_common_tokens)
+import neattext.functions as nfx
+from wordcloud import WordCloud
 
 
 def plot_wordcloud(docx):
-	mywordcloud = WordCloud().generate(docx)
-	plt.imshow(mywordcloud,interpolation='bilinear')
-	plt.axis('off')
-	st.pyplot()
+    mywordcloud = WordCloud().generate(docx)
+    plt.imshow(mywordcloud, interpolation="bilinear")
+    plt.axis("off")
+    st.pyplot()
 
 
 def plot_mendelhall_curve(docx):
-	word_length = [ len(token) for token in docx.split()]
-	word_length_count = Counter(word_length)
-	sorted_word_length_count = sorted(dict(word_length_count).items())
-	x,y = zip(*sorted_word_length_count)
-	fig = plt.figure(figsize=(20,10))
-	plt.plot(x,y)
-	plt.title("Plot of Word Length Distribution")
-	plt.show()
-	st.pyplot(fig)
-
-def visualize_text(docx):
-	"""Visualize Text Supplied
-	
-	Returns: several plots such as wordcloud,mendelhall_curve,pos_tags plot etc
-	"""
-	pass
+    word_length = [len(token) for token in docx.split()]
+    word_length_count = Counter(word_length)
+    sorted_word_length_count = sorted(dict(word_length_count).items())
+    x, y = zip(*sorted_word_length_count)
+    fig = plt.figure(figsize=(20, 10))
+    plt.plot(x, y)
+    plt.title("Plot of Word Length Distribution")
+    plt.show()
+    st.pyplot(fig)
 
 
-def summarize_text(docx,num_sentence=2):
-	"""Summarize Text
-	
-	Returns: a summary of text as either textrank or lexrank extractive form of summary
-	"""
-	# For Strings
-	parser = PlaintextParser.from_string(docx,Tokenizer("english"))
-	# Using LexRank
-	summarizer_lex = LexRankSummarizer()
-	summarizer_luhn = LuhnSummarizer()
-	summarizer_lsa = LsaSummarizer()
+def get_most_common_tokens(docx, num=10):
+    clean_docx = nfx.remove_stopwords(docx)
+    word_freq = Counter(clean_docx.split())
+    most_common_tokens = word_freq.most_common(num)
+    x, y = zip(*most_common_tokens)
+    return x, y
 
-	# Summarize Docs
-	summary_for_lex =summarizer_lex(parser.document,num_sentence)
-	summary_for_luhn =summarizer_luhn(parser.document,num_sentence)
-	summary_for_lsa =summarizer_lsa(parser.document,num_sentence)
 
-	# Results
-	summary_results = {'lexrank':summary_for_lex,'luhn':summary_for_luhn,'lsa':summary_for_lsa}
+def get_stopwords_freq(docx, num=10):
+    stopwords_list = nfx.extract_stopwords(docx)
+    word_freq = Counter(stopwords_list)
+    most_common_tokens = word_freq.most_common(num)
+    x, y = zip(*most_common_tokens)
+    return x, y
 
-	return summary_results
-		
+
+def get_mendelhall_curve(docx):
+    word_length = [len(token) for token in docx.split()]
+    word_length_count = Counter(word_length)
+    sorted_word_length_count = sorted(dict(word_length_count).items())
+    x, y = zip(*sorted_word_length_count)
+    return x, y
+
+
+def __get_wordcloud(docx):
+    mywordcloud = WordCloud().generate(docx)
+    return mywordcloud
+
+
+def get_most_common_pos_tags(docx, num=10):
+    blob = TextBlob(docx)
+    word, tag = zip(*blob.tags)
+    # Get most common
+    tag_freq = Counter(tag).most_common(num)
+    x, y = zip(*tag_freq)
+    return x, y
+
+
+def visualize_text(docx, token_num=10, figsize=(20, 10), rotation=40):
+    """Visualize Text Supplied
+
+    Returns: several plots such as wordcloud,mendelhall_curve,pos_tags plot etc
+    Params:
+            docx: text document
+            token_num: number of tokens to use.Default is 10
+            figsize: figsize as a tuple(20,10)
+            rotation: labelrotation for labels for only bar plot
+
+    Usage::
+    >>> visualize_text(docx)
+
+    """
+    # Generate Values for each metric
+    xt, yt = get_most_common_tokens(docx, num=token_num)
+    xs, ys = get_stopwords_freq(docx, num=token_num)
+    xm, ym = get_mendelhall_curve(docx)
+    xp, yp = get_most_common_pos_tags(docx)
+    mywordcloud = __get_wordcloud(docx)
+    # Vertically Stacked Subplot
+    # fig, ((ax1, ax2),(ax3, ax4)) = plt.subplots(2,2,figsize=figsize) # change to 3,2
+    fig, ((ax1, ax2), (ax3, ax4), (ax5, ax6)) = plt.subplots(3, 2, figsize=figsize)
+    fig.suptitle("TextViz Subplots[Vertical]")
+    ax1.bar(xt, yt)
+    ax1.set_title("Tokens Frequency")
+    ax1.tick_params(labelrotation=rotation)
+    ax2.bar(xs, ys)
+    ax2.set_title("Stopwords Frequency")
+    ax2.tick_params(labelrotation=rotation)
+    ax3.plot(xm, ym)
+    ax3.set_title("Mendalhall Curve")
+    ax4.imshow(mywordcloud, interpolation="bilinear")
+    ax4.set_title("Wordcloud")
+    ax5.bar(xp, yp, color="green")
+    ax5.set_title("PoS Tags Frequency")
+    ax5.tick_params(labelrotation=rotation)
+    ax6.bar(xs, ys)
+    ax6.set_title("Stopwords Frequency")
+    ax6.tick_params(labelrotation=rotation)
+
+
+def summarize_text(docx, num_sentence=2):
+    """Summarize Text
+
+    Returns: a summary of text as either textrank or lexrank extractive form of summary
+    """
+    # For Strings
+    parser = PlaintextParser.from_string(docx, Tokenizer("english"))
+    # Using LexRank
+    summarizer_lex = LexRankSummarizer()
+    summarizer_luhn = LuhnSummarizer()
+    summarizer_lsa = LsaSummarizer()
+
+    # Summarize Docs
+    summary_for_lex = summarizer_lex(parser.document, num_sentence)
+    summary_for_luhn = summarizer_luhn(parser.document, num_sentence)
+    summary_for_lsa = summarizer_lsa(parser.document, num_sentence)
+
+    # Results
+    summary_results = {
+        "lexrank": summary_for_lex,
+        "luhn": summary_for_luhn,
+        "lsa": summary_for_lsa,
+    }
+
+    return summary_results
+
 
 def analyze_text(docx):
-	"""Analyze Text lexically
-
-	"""
-	pass
+    """Analyze Text lexically"""
+    pass
 
 
 # Source https://github.com/adashofdata/nlp-in-python-tutorial/blob/master/5-Text-Generation.ipynb
@@ -127,9 +204,8 @@ def generate_text(text, num_of_words=15):
     >>> generate_text(t1,20)
     #Alternatively generate 4 sentences
     >>> for i in range(4):
-            print(generate_text(t1,15))
+                    print(generate_text(t1,15))
     """
     result_dict = markov_chain(text)
     final_result_sentence = __generate_text(result_dict, num_of_words)
     return final_result_sentence
-
--- textcaret/textcaret.py
+++ textcaret/textcaret.py
@@ -5,167 +5,200 @@
 
 from collections import Counter
 import matplotlib.pyplot as plt
-from wordcloud import WordCloud 
+from wordcloud import WordCloud
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.summarizers.lex_rank import LexRankSummarizer
 from sumy.summarizers.lsa import LsaSummarizer
 from sumy.summarizers.luhn import LuhnSummarizer
 from textblob import TextBlob
-
+from textcaret.functions import visualize_text
 
 
 class TextViz(object):
-	"""docstring for TextViz"""
-	def __init__(self, text):
-		super(TextViz, self).__init__()
-		self.text = text
+    """docstring for TextViz
 
+    Usage::
+    >>> from textcaret import TextViz
+    >>> docx = " your text document "
+    >>> tv = TextViz(docx)
+    >>> tv.visualize()
 
-	def plot_most_common_tokens(self,num=10):
-		word_freq = Counter(self.text.split())
-		most_common_tokens = word_freq.most_common(num)
-		x,y = zip(*most_common_tokens)
-		fig = plt.figure(figsize=(20,10))
-		plt.bar(x,y)
-		plt.title("Most Common Tokens")
-		plt.xticks(rotation=45)
-		plt.show()
+    """
 
+    def __init__(self, text):
+        super(TextViz, self).__init__()
+        self.text = text
 
-	def plot_wordcloud(self):
-		mywordcloud = WordCloud().generate(self.text)
-		plt.imshow(mywordcloud,interpolation='bilinear')
-		plt.axis('off')
-		plt.show(block=True)
-		
+    def __repr__(self):
+        return "TextViz(text={})".format(self.text)
 
+    def __str__(self):
+        return self.text
 
-	def plot_mendelhall_curve(docx):
-		word_length = [ len(token) for token in docx.split()]
-		word_length_count = Counter(word_length)
-		sorted_word_length_count = sorted(dict(word_length_count).items())
-		x,y = zip(*sorted_word_length_count)
-		fig = plt.figure(figsize=(20,10))
-		plt.plot(x,y)
-		plt.title("Plot of Word Length Distribution")
-		plt.show()
-	
-		
-		
+    def plot_most_common_tokens(self, num=10):
+        word_freq = Counter(self.text.split())
+        most_common_tokens = word_freq.most_common(num)
+        x, y = zip(*most_common_tokens)
+        fig = plt.figure(figsize=(20, 10))
+        plt.bar(x, y)
+        plt.title("Most Common Tokens")
+        plt.xticks(rotation=45)
+        plt.show()
 
+    def plot_wordcloud(self):
+        mywordcloud = WordCloud().generate(self.text)
+        plt.imshow(mywordcloud, interpolation="bilinear")
+        plt.axis("off")
+        plt.show(block=True)
 
-class TextCaret(object):
-	"""docstring for TextCaret"""
-	def __init__(self,text):
-		super(TextCaret, self).__init__()
-		self.text = text
+    def plot_mendelhall_curve(self):
+        word_length = [len(token) for token in self.text.split()]
+        word_length_count = Counter(word_length)
+        sorted_word_length_count = sorted(dict(word_length_count).items())
+        x, y = zip(*sorted_word_length_count)
+        fig = plt.figure(figsize=(20, 10))
+        plt.plot(x, y)
+        plt.title("Plot of Word Length Distribution")
+        plt.show()
 
-	def __repr__(self):
-		return 'TextCaret(text={})'.format(self.text)
-
-	def __str__(self):
-		return self.text 
-
-
-	def prepare(self,stopwords=False,special_char=False,punctuations=False):
-		import neattext.functions as nfx
-		if stopwords == True:
-			self.text = nfx.remove_stopwords(self.text)
-		if special_char == True:
-			self.text = nfx.remove_special_characters(self.text)
-		else:
-			self.text = self.text 
-
-		prepared_text = self.text
-		return prepared_text
-
-	def visualize(self):
-		"""Visualize The Given Text"""
-		new_docx = TextViz(self.text)
-		# Plot word cloud
-		return new_docx.plot_wordcloud()
+    def visualize(self):
+        return visualize_text(self.text)
 
 
 class TextSummarizer(object):
-	"""TextSummarizer: summarize a given document/text using several summarization
-	algorithms such as lexrank,luhn,lsa,etc
-	
-	Returns: A Dictionary of various summary per the each extractive algorithm
-	
-	Usage::
-	>>> from textcaret import TextSummarizer
-	>>> s = "your text"
-	>>> summarizer = TextSummarizer(s)
-	>>> summarizer.summarize()
+    """TextSummarizer: summarize a given document/text using several summarization
+    algorithms such as lexrank,luhn,lsa,etc
 
-	"""
-	def __init__(self, text=None):
-		super(TextSummarizer, self).__init__()
-		self.text = text
+    Returns: A Dictionary of various summary per the each extractive algorithm
 
-	def __repr__(self):
-		return 'TextSummarizer(text={})'.format(self.text)
+    Usage::
+    >>> from textcaret import TextSummarizer
+    >>> s = "your text"
+    >>> summarizer = TextSummarizer(s)
+    >>> summarizer.summarize()
 
-	def __str__(self):
-		return self.text 
+    """
 
-	def summarize(self,num_sentence=2):
-		docx = self.text
-		# For Strings
-		parser = PlaintextParser.from_string(docx,Tokenizer("english"))
-		# Using LexRank
-		summarizer_lex = LexRankSummarizer()
-		summarizer_luhn = LuhnSummarizer()
-		summarizer_lsa = LsaSummarizer()
+    def __init__(self, text=None):
+        super(TextSummarizer, self).__init__()
+        self.text = text
 
-		# Summarize Docs
-		summary_for_lex =summarizer_lex(parser.document,num_sentence)
-		summary_for_luhn =summarizer_luhn(parser.document,num_sentence)
-		summary_for_lsa =summarizer_lsa(parser.document,num_sentence)
+    def __repr__(self):
+        return "TextSummarizer(text={})".format(self.text)
 
-		# Results
-		summary_results = {'lexrank':summary_for_lex,'luhn':summary_for_luhn,'lsa':summary_for_lsa}
+    def __str__(self):
+        return self.text
 
-		return summary_results
+    def summarize(self, num_sentence=2):
+        docx = self.text
+        # For Strings
+        parser = PlaintextParser.from_string(docx, Tokenizer("english"))
+        # Using LexRank
+        summarizer_lex = LexRankSummarizer()
+        summarizer_luhn = LuhnSummarizer()
+        summarizer_lsa = LsaSummarizer()
+
+        # Summarize Docs
+        summary_for_lex = summarizer_lex(parser.document, num_sentence)
+        summary_for_luhn = summarizer_luhn(parser.document, num_sentence)
+        summary_for_lsa = summarizer_lsa(parser.document, num_sentence)
+
+        # Results
+        summary_results = {
+            "lexrank": summary_for_lex,
+            "luhn": summary_for_luhn,
+            "lsa": summary_for_lsa,
+        }
+
+        return summary_results
 
 
 class TextSentiment(object):
-	"""docstring for TextSentiment"""
-	def __init__(self, text,split_sentence=False):
-		super(TextSentiment, self).__init__()
-		self.text = text
-		self.split_sentence = False
+    """docstring for TextSentiment"""
 
-	def __repr__(self):
-		return 'TextSentiment(text="{}",split_sentence="{}")'.format(self.text,self.split_sentence)
+    def __init__(self, text, split_sentence=False):
+        super(TextSentiment, self).__init__()
+        self.text = text
+        self.split_sentence = False
 
-	def __str__(self):
-		return self.text 
+    def __repr__(self):
+        return 'TextSentiment(text="{}",split_sentence="{}")'.format(
+            self.text, self.split_sentence
+        )
 
-	def sentiment(self):
-		if self.split_sentence == True:
-			sentence_tokens = [sent for sent in str(self.text).split('.')]
-			sentiment_list = []
-			for sent in sentence_tokens:
-				sentiment = TextBlob(sent).sentiment
-				results = (sent,sentiment.polarity) 
-				sentiment_list.append(results)
-			sentiment_results = {'sentiment':sentiment_list}
-		else:
-			blob = TextBlob(self.text)
-			sentiment = blob.sentiment
-			sentiment_results = {'sentence':self.text,'sentiment':sentiment}
-		return sentiment_results
+    def __str__(self):
+        return self.text
 
-
-		
-
-
+    def sentiment(self):
+        if self.split_sentence == True:
+            sentence_tokens = [sent for sent in str(self.text).split(".")]
+            sentiment_list = []
+            for sent in sentence_tokens:
+                sentiment = TextBlob(sent).sentiment
+                results = (sent, sentiment.polarity)
+                sentiment_list.append(results)
+            sentiment_results = {"sentiment": sentiment_list}
+        else:
+            blob = TextBlob(self.text)
+            sentiment = blob.sentiment
+            sentiment_results = {"sentence": self.text, "sentiment": sentiment}
+        return sentiment_results
 
 
-		
+class TextCaret(TextViz, TextSummarizer, TextSentiment):
+    """TextCaret: main object for nlp task
 
-		
+    Returns: TextCaret Object
 
-		
+    Usage::
+    >>> from textcaret import TextCaret
+    >>> s = "your text"
+    >>> docx = TextCaret(s)
+    >>> docx.visual_report()
+    >>> docx.summary_report()
+    >>> docx.sentiment_report()
+
+    """
+
+    def __init__(self, text):
+        super(TextCaret, self).__init__()
+        self.text = text
+
+    def __repr__(self):
+        return "TextCaret(text={})".format(self.text)
+
+    def __str__(self):
+        return self.text
+
+    def prepare(self, stopwords=False, special_char=False, punctuations=False):
+        import neattext.functions as nfx
+
+        if stopwords == True:
+            self.text = nfx.remove_stopwords(self.text)
+        if special_char == True:
+            self.text = nfx.remove_special_characters(self.text)
+        else:
+            self.text = self.text
+
+        prepared_text = self.text
+        return prepared_text
+
+    def visual_report(self):
+        """Visualize The Given Text"""
+        new_docx = TextViz(self.text)
+        # Generate Plots
+        return new_docx.visualize()
+
+    def summary_report(self):
+        """Summarize the Given Text"""
+        new_docx = TextSummarizer(self.text)
+        # Generate Summary
+        return new_docx.summarize()
+
+    def sentiment_report(self):
+        """Perform Sentiment Analysis on Text"""
+
+        new_docx = TextSentiment(self.text)
+        # Generate Sentiment Report
+        return new_docx.sentiment
